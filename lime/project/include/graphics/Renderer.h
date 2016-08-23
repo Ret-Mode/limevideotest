4af1eca753e2ea4916fc86be568f4b1f95568751
RetardMode <RetardMode@users.noreply.github.com>
2016-08-23 21:06:38 +0200

    Update Renderer.h

--- lime/project/include/graphics/Renderer.h
+++ lime/project/include/graphics/Renderer.h
@@ -25,7 +25,7 @@ namespace lime {
 			virtual const char* Type () = 0;
 			virtual void Unlock () = 0;
 			virtual void CreateMovieTexture(unsigned int w, unsigned int h)=0;
-			virtual void UpdateMovieTexture(unsigned char* y, int ystride, unsigned char *cb, int cbstride, unsigned char *cr, int crstride)=0;
+			virtual void UpdateMovieTexture(int scale, unsigned char* y, int ystride, unsigned char *cb, int cbstride, unsigned char *cr, int crstride)=0;
 			virtual void DestroyMovieTexture()=0;
 			Window* currentWindow;
 		
