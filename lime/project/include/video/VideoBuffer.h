1bf87b3c90dad9320da71ef556c0b4a980dca31f
RetardMode <RetardMode@users.noreply.github.com>
2016-08-28 11:37:32 +0200

    Android fix

--- lime/project/include/video/VideoBuffer.h
+++ lime/project/include/video/VideoBuffer.h
@@ -1,14 +1,35 @@
 #ifndef VIDEOBUFFER_H
 #define VIDEOBUFFER_H
 
+
+#ifdef ANDROID
+#include <android/log.h>
+#endif
+
+
+#ifdef ANDROID
+#define LOG_VIDEO(args,...) __android_log_print(ANDROID_LOG_INFO, "Lime", args, ##__VA_ARGS__)
+#else
+#ifdef IPHONE
+//#define LOG_VIDEO(args,...) printf(args, ##__VA_ARGS__)
+//#define LOG_VIDEO(args...) { }
+#elif defined(TIZEN)
+#include <FBase.h>
+#define LOG_VIDEO(args,...) AppLog(args, ##__VA_ARGS__)
+#else
+#define LOG_VIDEO(args,...) printf(args, ##__VA_ARGS__)
+#endif
+#endif
+
 #include <system/System.h>
 #include <vector>
+#include <utils/Resource.h>
 
 namespace lime {
 	class VideoBuffer {
 	public:
 		
-			virtual int testVideo(const char * const file, int scale, int delay)=0;/**/
+			virtual int testVideo(Resource *resource, int scale, int delay)=0;/**/
 			virtual void playVideo(double timestamp)=0;/**/
 			virtual int processVideo(double timestamp)=0;/**/
 			virtual void cleanVideo()=0;/**/
