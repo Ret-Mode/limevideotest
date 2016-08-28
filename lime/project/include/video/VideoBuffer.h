d8a9e7c005d1354d047f4a3732e43b6eafcef382
RetardMode <RetardMode@users.noreply.github.com>
2016-08-28 15:42:17 +0200

    Fix broken Mac define (?)

--- lime/project/include/video/VideoBuffer.h
+++ lime/project/include/video/VideoBuffer.h
@@ -12,7 +12,7 @@
 #else
 #ifdef IPHONE
 //#define LOG_VIDEO(args,...) printf(args, ##__VA_ARGS__)
-//#define LOG_VIDEO(args...) { }
+#define LOG_SOUND(args...) { }
 #elif defined(TIZEN)
 #include <FBase.h>
 #define LOG_VIDEO(args,...) AppLog(args, ##__VA_ARGS__)
